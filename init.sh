63849eff50d918803620c169b81685725fef8ff2
pedevineau <pedevineau@hotmail.fr>
2022-09-30 15:51:38 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@
 
 WORK_DIR=/home/jovyan/work
 CLONE_DIR=${WORK_DIR}/explainable-demo
-
+ENV_DIR=${WORK_DIR}/explainable-env
 # Clone course repository
 REPO_URL=https://github.com/pedevineau/explainable-demo.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
+pip install virtualenv
+virtualenv $ENV_DIR
+source ${ENV_DIR}/bin/activate
+
 # Install additional packages if needed
 REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE && rm $REQUIREMENTS_FILE
+
 
 # Remove course Git repository
 #rm -r $CLONE_DIR
