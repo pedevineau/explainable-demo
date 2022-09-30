1ea143d8384a4c2e7534302d2dac4246f5ca922b
pedevineau <pedevineau@hotmail.fr>
2022-09-30 17:26:42 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-WORK_DIR=/home/onyxia/work
+SESSION=onyxia
+WORK_DIR=/home/${SESSION}/work
 CLONE_DIR=${WORK_DIR}/explainable-demo
 ENV_DIR=${WORK_DIR}/explainable-env
 # Clone course repository
 REPO_URL=https://github.com/pedevineau/explainable-demo.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
-pip install virtualenv
+pip install virtualenv p2j
+p2j book.py
 virtualenv $ENV_DIR
 source ${ENV_DIR}/bin/activate
 
@@ -16,7 +18,8 @@ REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE && rm $REQUIREMENTS_FILE
 
 
+
 # Remove course Git repository
 #rm -r $CLONE_DIR
 
-chown jovyan -R /home/jovyan/ # Otherwise jovyan has no rights on the local repo folder.
+chown ${SESSION} -R /home/${SESSION}/ # Otherwise jovyan has no rights on the local repo folder.
